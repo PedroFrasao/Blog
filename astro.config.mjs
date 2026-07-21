fa1d71edbd643394b5bcd98fee4e04fdfc880b29
Pedro <chais1eee2@gmail.com>
2026-07-20 22:20:48 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // Troque pela URL final do seu site no Cloudflare Pages (ou seu domínio próprio)
 export default defineConfig({
-  site: 'https://meu-blog.pages.dev',
+  site: 'https://blogg.chais1eee2.workers.dev',
 });
 
